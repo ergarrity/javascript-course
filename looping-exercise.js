56c0dd0d25a1db0ea9dc114c697c30474f2d3170
Liz Garrity <ergarrity@gmail.com>
2018-12-30 18:18:47 -0800

    Completed looping exercise part 2: loop through all properties of suspect objects in suspects array.

--- looping-exercise.js
+++ looping-exercise.js
@@ -1,17 +1,43 @@
-// Loop through the suspects array
+// Part 1: Loop through the suspects array
 
 const game = {
     'suspects': [
         {
             name: "Rusty",
-            color: "orange"
+            color: "orange",
+            guess: "not guilty"
         }, {
             name: "Miss Scarlet",
-            color: "red"
+            color: "red",
+            guess: "guilty"
         }
     ]
 }
 
-for ( i = 0; i < game.suspects.length; i++){
-    console.log(game.suspects[i]);
+function printSuspects() {
+    for (let i = 0; i < game.suspects.length; i++){
+        console.log(game.suspects[i]);
+    }
 }
+
+// printSuspects();
+
+// Part 2: Loop through all of the properties of the suspects
+// object in the suspects array. Mark them if you think they
+// are guilty.
+
+function printSuspectProperties() {
+    
+    for (let i = 0; i < game.suspects.length; i++){
+        
+        console.log("SUSPECT " + (i+1) + ":");
+        
+        let suspect = game.suspects[i];
+        
+        for (let property in suspect) {
+            console.log("    " +property + ": " + suspect[property]);
+        }
+    }
+}
+
+printSuspectProperties();
